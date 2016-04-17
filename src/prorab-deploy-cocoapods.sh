989af3fd01419af4acf5b41716b9e14dfc9562e0
Ivan Gagis <igagis@gmail.com>
2016-04-17 13:29:50 +0300

    release 1.7.4

--- src/prorab-deploy-cocoapods.sh
+++ src/prorab-deploy-cocoapods.sh
@@ -31,4 +31,4 @@ cutSecret="sed -n -e s/$HOMEBREW_GITHUB_ACCESS_TOKEN/<secret>/p"
 #Need to pass --use-libraries because before pushing the spec it will run 'pod lint'
 #on it. And 'pod lint' uses framework integration by default which will fail to copy
 #some header files to the right places.
-pod repo push $1 cocoapods/*.podspec --use-libraries --allow-warnings | $cutSecret
+pod repo push $1 cocoapods/*.podspec --use-libraries --allow-warnings 2>&1 | $cutSecret
