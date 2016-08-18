bfdca6eb1d49b0ccaf3f1ea12c732c85818e8c35
Ivan Gagis <igagis@gmail.com>
2016-08-18 16:11:07 +0300

    added prorab-build-static-lib rules

--- src/prorab.mk
+++ src/prorab.mk
@@ -1,5 +1,4 @@
-# Prorab build system
-# Copyright Ivan Gagis <igagis@gmail.com>
+# prorab - the build system
 
 
 #once
@@ -101,6 +100,8 @@ ifneq ($(prorab_included),true)
 
 
     define prorab-private-app-specific-rules
+        #need empty line here to avoid merging with adjacent macro instantiations
+
         $(eval prorab_private_ldflags := )
 
         $(if $(filter windows,$(prorab_os)), \
@@ -117,11 +118,15 @@ ifneq ($(prorab_included),true)
 
         uninstall::
 		$(prorab_echo)rm -f $(DESTDIR)$(PREFIX)/bin/$(notdir $(prorab_this_name))
+
+        #need empty line here to avoid merging with adjacent macro instantiations
     endef
 
 
 
-    define prorab-private-lib-specific-rules-nix-systems
+    define prorab-private-dynamic-lib-specific-rules-nix-systems
+        #need empty line here to avoid merging with adjacent macro instantiations
+
         $(if $(filter macosx,$(prorab_os)), \
                 $(eval prorab_this_symbolic_name := $(abspath $(prorab_this_dir)lib$(this_name)$(prorab_lib_extension))) \
                 $(eval prorab_this_name := $(abspath $(prorab_this_dir)lib$(this_name).$(this_soname)$(prorab_lib_extension))) \
@@ -148,10 +153,14 @@ ifneq ($(prorab_included),true)
 
         clean::
 		$(prorab_echo)rm -f $(prorab_this_symbolic_name)
+
+        #need empty line here to avoid merging with adjacent macro instantiations
     endef
 
     
     define prorab-private-lib-install-headers-rule
+        #need empty line here to avoid merging with adjacent macro instantiations
+
         #foreach is used to filter out all files which are not inside of a directory
         $(eval prorab_private_headers := $(foreach v,$(patsubst $(prorab_this_dir)%,%,$(shell find $(prorab_this_dir) -type f -name "*.hpp" -o -name "*.h")),$(if $(findstring /,$(v)),$(v),)))
 
@@ -165,51 +174,62 @@ ifneq ($(prorab_included),true)
 		$(prorab_echo)for i in $(dir $(prorab_private_headers)); do \
 		    rm -rf $(DESTDIR)$(PREFIX)/include/$$$$i; \
 		done
+
+        #need empty line here to avoid merging with adjacent macro instantiations
     endef
 
-    define prorab-private-lib-specific-rules
+    define prorab-private-dynamic-lib-specific-rules
+        #need empty line here to avoid merging with adjacent macro instantiations
+
         $(if $(filter windows,$(prorab_os)), \
                 $(eval prorab_this_name := $(abspath $(prorab_this_dir)lib$(this_name)$(prorab_lib_extension))) \
                 $(eval prorab_private_ldflags := -shared -s) \
                 $(eval prorab_this_symbolic_name := $(prorab_this_name)) \
             , \
-                $(prorab-private-lib-specific-rules-nix-systems) \
+                $(prorab-private-dynamic-lib-specific-rules-nix-systems) \
             )
 
-        $(eval prorab_this_staticlib := $(abspath $(prorab_this_dir)lib$(this_name).a))
-
-
-        all: $(prorab_this_staticlib)
-
-        clean::
-		$(prorab_echo)rm -f $(prorab_this_staticlib)
-
-        $(prorab-private-lib-install-headers-rule)
-
-        install:: $(prorab_this_staticlib) $(prorab_this_name)
+        install:: $(prorab_this_name)
 		$(prorab_echo)install -d $(DESTDIR)$(PREFIX)/lib/
-		$(prorab_echo)install -m 644 $(prorab_this_staticlib) $(DESTDIR)$(PREFIX)/lib/
 		$(prorab_echo)install $(prorab_this_name) $(DESTDIR)$(PREFIX)/lib/
 		$(if $(filter macosx,$(prorab_os)), \
 		        $(prorab_echo)install_name_tool -id "$(PREFIX)/lib/$(notdir $(prorab_this_name))" $(DESTDIR)$(PREFIX)/lib/$(notdir $(prorab_this_name)) \
 		    )
 
         uninstall::
-		$(prorab_echo)rm -f $(DESTDIR)$(PREFIX)/lib/$(notdir $(prorab_this_staticlib))
 		$(prorab_echo)rm -f $(DESTDIR)$(PREFIX)/lib/$(notdir $(prorab_this_name))
+	
+        #need empty line here to avoid merging with adjacent macro instantiations
     endef
 
     define prorab-private-lib-static-library-rule
+        #need empty line here to avoid merging with adjacent macro instantiations
+
+        $(eval prorab_this_staticlib := $(abspath $(prorab_this_dir)lib$(this_name).a))
+
+        all: $(prorab_this_staticlib)
+
+        clean::
+		$(prorab_echo)rm -f $(prorab_this_staticlib)
+
+        install:: $(prorab_this_staticlib)
+		$(prorab_echo)install -d $(DESTDIR)$(PREFIX)/lib/
+		$(prorab_echo)install -m 644 $(prorab_this_staticlib) $(DESTDIR)$(PREFIX)/lib/
+
+        uninstall::
+		$(prorab_echo)rm -f $(DESTDIR)$(PREFIX)/lib/$(notdir $(prorab_this_staticlib))
+
         #static library rule
         $(prorab_this_staticlib): $(prorab_this_objs) $(prorab_this_makefile)
-			@echo "Creating static library $$(notdir $$@)..."
-			$(prorab_echo)ar cr $$@ $$(filter %.o,$$^)
+		@echo "Creating static library $$(notdir $$@)..."
+		$(prorab_echo)ar cr $$@ $$(filter %.o,$$^)
+
+        #need empty line here to avoid merging with adjacent macro instantiations
     endef
 
 
-    define prorab-private-common-rules
-
-        all: $(prorab_this_name)
+    define prorab-private-compile-rules
+        #need empty line here to avoid merging with adjacent macro instantiations
 
         #calculate max number of steps up in source paths and prepare obj directory spacer
         $(eval prorab_private_numobjspacers := )
@@ -239,43 +259,76 @@ ifneq ($(prorab_included),true)
         #include rules for header dependencies
         include $(wildcard $(addsuffix *.d,$(dir $(prorab_this_objs))))
 
+        clean::
+		$(prorab_echo)rm -rf $(prorab_this_dir)$(prorab_obj_dir)
+
+        #need empty line here to avoid merging with adjacent macro instantiations
+    endef
+
+    define prorab-private-link-rules
+        #need empty line here to avoid merging with adjacent macro instantiations
+
+        all: $(prorab_this_name)
+
         #link rule
         $(prorab_this_name): $(prorab_this_objs) $(prorab_this_makefile)
 		@echo "Linking $$@..."
 		$(prorab_echo)$$(CC) $$(filter %.o,$$^) -o "$$@" $(this_ldlibs) $(this_ldflags) $(LDLIBS) $(LDFLAGS) $(prorab_private_ldflags)
 
-        #clean rule
         clean::
-		$(prorab_echo)rm -rf $(prorab_this_dir)$(prorab_obj_dir)
 		$(prorab_echo)rm -f $(prorab_this_name)
+
+        #need empty line here to avoid merging with adjacent macro instantiations
     endef
 
     #if there are no any sources in this_srcs then just install headers, no need to build binaries
     define prorab-build-lib
+        #need empty line here to avoid merging with adjacent macro instantiations
+
+        $(prorab-build-static-lib)
+        $(prorab-private-dynamic-lib-specific-rules)
+        $(prorab-private-link-rules)
+
+        #need empty line here to avoid merging with adjacent macro instantiations
+    endef
+
+    define prorab-build-static-lib
+        #need empty line here to avoid merging with adjacent macro instantiations
+
+        $(prorab-private-lib-install-headers-rule)
         $(if $(this_srcs), \
-                $(prorab-private-lib-specific-rules) \
-                $(prorab-private-common-rules) \
+                $(prorab-private-compile-rules) \
                 $(prorab-private-lib-static-library-rule) \
                 , \
-                $(prorab-private-lib-install-headers-rule) \
             )
+
+        #need empty line here to avoid merging with adjacent macro instantiations
     endef
 
 
     define prorab-build-app
+        #need empty line here to avoid merging with adjacent macro instantiations
+
         $(prorab-private-app-specific-rules)
-        $(prorab-private-common-rules)
+        $(prorab-private-compile-rules)
+        $(prorab-private-link-rules)
+
+        #need empty line here to avoid merging with adjacent macro instantiations
     endef
 
 
 
 
     define prorab-include
+        #need empty line here to avoid merging with adjacent macro instantiations
+
         $(if $(filter $(abspath $1),$(prorab_included_makefiles)), \
             , \
                 $(eval prorab_included_makefiles += $(abspath $1)) \
                 $(call prorab-private-include,$1) \
             )
+
+        #need empty line here to avoid merging with adjacent macro instantiations
     endef
 
 
@@ -284,6 +337,8 @@ ifneq ($(prorab_included),true)
 
     #include file with correct prorab_this_dir
     define prorab-private-include
+        #need empty line here to avoid merging with adjacent macro instantiations
+
         prorab_private_this_makefiles += $$(prorab_this_makefile)
         prorab_this_makefile := $1
         prorab_this_dir := $$(dir $$(prorab_this_makefile))
@@ -292,12 +347,17 @@ ifneq ($(prorab_included),true)
         prorab_this_dir := $$(dir $$(prorab_this_makefile))
         prorab_private_this_makefiles := $$(wordlist 1,$$(call prorab-num,$$(call prorab-dec,$$(prorab_private_this_makefiles))),$$(prorab_private_this_makefiles))
 
+        #need empty line here to avoid merging with adjacent macro instantiations
     endef
     #!!!NOTE: the trailing empty line in 'prorab-private-include' definition is needed so that include files would be separated from each other
 
     #include all makefiles in subdirectories
     define prorab-build-subdirs
+        #need empty line here to avoid merging with adjacent macro instantiations
+
         $(foreach path,$(wildcard $(prorab_this_dir)*/makefile),$(call prorab-include,$(path)))
+
+        #need empty line here to avoid merging with adjacent macro instantiations
     endef
 
 
@@ -307,6 +367,8 @@ ifneq ($(prorab_included),true)
 
     #doxygen docs are only possible for libraries, so install path is lib*-doc
     define prorab-build-doxygen
+        #need empty line here to avoid merging with adjacent macro instantiations
+
         all: doc
 
         doc:: $(prorab_this_dir)doxygen
@@ -329,14 +391,20 @@ ifneq ($(prorab_included),true)
 
         uninstall::
 		$(prorab_echo)rm -rf $(DESTDIR)$(PREFIX)/share/doc/lib$(this_name)-doc
+
+        #need empty line here to avoid merging with adjacent macro instantiations
     endef
 
 
     define prorab-pkg-config
+        #need empty line here to avoid merging with adjacent macro instantiations
+
         install:: $(shell ls $(prorab_this_dir)*.pc.in)
 		$(prorab_echo)prorab-apply-version.sh -v `prorab-deb-version.sh $(prorab_this_dir)../debian/changelog` $(prorab_this_dir)*.pc.in
 		$(prorab_echo)install -d $(DESTDIR)$(PREFIX)/lib/pkgconfig
 		$(prorab_echo)install -m 644 $(prorab_this_dir)*.pc $(DESTDIR)$(PREFIX)/lib/pkgconfig
+
+        #need empty line here to avoid merging with adjacent macro instantiations
     endef
 
     #define function to find all source files from specified directory recursively
