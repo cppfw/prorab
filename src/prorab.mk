cfc2e1a23b7cb950d03a135e4960f1eb91bb5c27
Ivan Gagis <igagis@gmail.com>
2016-09-12 14:15:02 +0300

    release 1.10.5

--- src/prorab.mk
+++ src/prorab.mk
@@ -446,7 +446,8 @@ ifneq ($(prorab_included),true)
     endef
 
     #define function to find all source files from specified directory recursively
-    prorab-src-dir = $(patsubst $(prorab_this_dir)%,%,$(shell find $(patsubst %/,%,$(prorab_this_dir)$1) -type f -name "*.cpp" -o -name "*.c"))
+    #NOTE: removing trailing '/' or '/.' before invoking 'find'.
+    prorab-src-dir = $(patsubst $(prorab_this_dir)%,%,$(shell find $(patsubst %/.,%,$(patsubst %/,%,$(prorab_this_dir)$1)) -type f -name "*.cpp" -o -name "*.c"))
 
 endif #~once
 
