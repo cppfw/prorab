da8379a51bac18b58c499bf1b17aca49ba60b2af
Ivan Gagis <igagis@gmail.com>
2016-12-22 16:33:25 +0200

    deploy to cocoapods script improved

--- src/prorab-deploy-cocoapods.sh
+++ src/prorab-deploy-cocoapods.sh
@@ -4,32 +4,42 @@
 #It assumes that cocoapods specs to deploy are in 'cocoapods' directory.
 
 
-while true; do
+while [[ $# > 0 ]] ; do
     case $1 in
         --help)
             echo "Usage:"
-            echo "	$(basename $0) <repo-name>"
+            echo "	$(basename $0) -r <repo-name> [<spec1.podspec.in> <spec2.podspec.in>...]"
             echo " "
             echo "Environment variable PRORAB_GIT_ACCESS_TOKEN can be set to git access token, so that it will be stripped out from the script output."
             echo " "
             echo "Example:"
-            echo "	$(basename $0) igagis"
+            echo "	$(basename $0) -r igagis cocoapods/*.podspec.in"
             exit 0
         ;;
-        *)
-            break
-        ;;
+        -r)
+			shift
+			reponame=$1
+			shift
+			;;
+		*)
+			infiles="$infiles $1"
+			shift
+			;;
     esac
 done
+
+if [ -z "$infiles" ]; then
+	infiles=$(ls cocoapods/*.rb.in)
+fi
 
 echo "Deploying to cocoapods..."
 
 #update version numbers
 version=$(prorab-deb-version.sh debian/changelog)
 
-echo "current package version is $version, applying it to podspec..."
+echo "current package version is $version, applying it to podspecs..."
 
-prorab-apply-version.sh -v $version cocoapods/*.podspec.in
+prorab-apply-version.sh -v $version $infiles
 
 echo "version $version applied to podspec"
 
@@ -38,9 +48,14 @@ echo "version $version applied to podspec"
 
 cutSecret="sed -e s/$PRORAB_GIT_ACCESS_TOKEN/<secret>/"
 
-#Need to pass --use-libraries because before pushing the spec it will run 'pod lint'
-#on it. And 'pod lint' uses framework integration by default which will fail to copy
-#some header files to the right places.
-set -o pipefail && pod repo push $1 cocoapods/*.podspec --use-libraries --allow-warnings 2>&1 | $cutSecret
+for fin in $infiles
+do
+    f=$(echo $fin | sed -n -e 's/\(.*\)\.in$/\1/p')
+
+	#Need to pass --use-libraries because before pushing the spec it will run 'pod lint'
+	#on it. And 'pod lint' uses framework integration by default which will fail to copy
+	#some header files to the right places.
+	set -o pipefail && pod repo push $reponame $f --use-libraries --allow-warnings 2>&1 | $cutSecret
+done
 
 echo "Deploying to cocoapods done!"
