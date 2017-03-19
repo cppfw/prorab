06590e8ace0002ea7b86ebb91823b3c4942fa2e3
Ivan Gagis <igagis@gmail.com>
2017-03-19 12:08:01 +0200

    adding prorab-running-test.sh script

---
+++ src/prorab-running-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+while [[ $# > 0 ]] ; do
+	case $1 in
+		--help)
+			echo "Print 'running test' message to the console."
+			echo ""
+			echo "Usage:"
+			echo -e "\t$(basename $0) \"test name\""
+			echo ""
+			echo "Examples:"
+			echo -e "\t$(basename $0) \"test1\""
+			exit 0
+			;;
+		*)
+			testName="$testName $1"
+			shift
+			;;
+	esac
+done
+
+[ -z "$testName" ] && echo "Error: no test name supplied" && exit 1;
+
+echo -e "\\033[0;31mRunning test\\033[0m$testName..."
--- tests/common.mk
+++ tests/common.mk
@@ -1,12 +1,8 @@
 this_err := $(d)../../src/prorab-error.sh
 this_pass := $(d)../../src/prorab-passed.sh
+this_running := $(d)../../src/prorab-running-test.sh
 
 this_dirs := $(subst /, ,$(d))
 this_test := $(word $(words $(this_dirs)),$(this_dirs))
 
-define this_executing_test_
-@echo "\\033[0;31mexecuting test\\033[0m $(this_test)..."
-endef
 
-#this is a workaround, for some reason verbatim define := does not work in make version 3.81, but works in 4.1 at least
-this_executing_test := $(this_executing_test_)
