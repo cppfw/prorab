e02488c4ada50ed3d1fdd017f1a174f46a27c117
Ivan Gagis <igagis@gmail.com>
2017-11-15 16:45:48 +0200

    release 1.10.50

--- src/prorab-deb-prepare.sh
+++ src/prorab-deb-prepare.sh
@@ -11,21 +11,21 @@ echo "Preparing Debian package for building..."
 soname=$(cat src/soname.txt 2>/dev/null || true) #ignore error if there is no soname.txt file
 
 if [ -z "$soname" ]; then
-	echo "no soname found, skipping applying soname..."
+	echo "no soname found, using empty soname"
+	soname=
 else
 	echo "Detected soname = $soname"
-
-	listOfInstalls=$(ls debian/*.install.in 2>/dev/null)
-
-	for i in $listOfInstalls; do
-		echo "Applying soname to $i..."
-		cp $i ${i%.install.in}$soname.install
-	done
-
-
-	echo "Applying soname to debian/control.in..."
-
-	sed -e "s/\$(soname)/$soname/g" debian/control.in > debian/control
 fi
+
+listOfInstalls=$(ls debian/*.install.in 2>/dev/null || true) #allow package without *.install.in
+
+for i in $listOfInstalls; do
+	echo "Applying soname to $i..."
+	cp $i ${i%.install.in}$soname.install
+done
+
+echo "Applying soname to debian/control.in..."
+
+sed -e "s/\$(soname)/$soname/g" debian/control.in > debian/control
 
 echo "Debian package prepared for building!"
