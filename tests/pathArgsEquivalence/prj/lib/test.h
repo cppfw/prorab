aac10eff63b56ab4888b7467bacc25abd49e43f1
Ivan Gagis <igagis@gmail.com>
2019-09-13 22:19:58 +0300

    added test case for library path arguments equivalence when building from different directories

---
+++ tests/pathArgsEquivalence/prj/lib/test.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   test.h
+ * Author: ivan
+ *
+ * Created on February 11, 2016, 3:57 PM
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int decrement(int arg);
+
+
+#ifdef __cplusplus
+}
+#endif
