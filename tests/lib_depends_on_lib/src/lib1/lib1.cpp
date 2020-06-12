da64432abd971654546da2f1934e63bd2f2466a7
Ivan Gagis <igagis@gmail.com>
2020-06-13 02:04:02 +0300

    lib_depends_on_lib test

---
+++ tests/lib_depends_on_lib/src/lib1/lib1.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void func_1(){
+	std::cout << "hello from func_1" << std::endl;
+}
---
+++ tests/lib_depends_on_lib/src/lib2/lib2.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void func_2(){
+	std::cout << "hello from func_2" << std::endl;
+}
