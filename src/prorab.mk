9617c95c8a6cdb6c598fadf0d8d41b94be638e75
Ivan Gagis <igagis@gmail.com>
2020-10-30 19:05:36 +0200

    add prorab-config macro

--- src/prorab.mk
+++ src/prorab.mk
@@ -246,6 +246,30 @@ ifneq ($(prorab_is_included),true)
         endif
     endif
 
+    #########################################
+    # macro for setting build configuration #
+
+    ifeq ($(config),)
+        override config := $(c)
+    else
+        override c := $(config)
+    endif
+	ifeq ($(config),)
+        override config := rel
+        override c := $(config)
+	endif
+
+    define prorab-config
+        $(if $1,,$(error no 'config dir' argument is given to prorab-config macro))
+
+        this_out_dir := out/$(c)/
+        $(eval config_dir := $(abspath $(d)$(filter-out ,$1))/) # filter-out is needed to trim possible leading and trailing spaces
+
+        $(eval prorab_private_config_file := $(config_dir)$(c).mk)
+        $(if $(wildcard $(prorab_private_config_file)),,$(error no $(c).mk config file found in $(filter-out ,$1) directory))
+        include $(prorab_private_config_file)
+    endef
+
     ################################
     # makefile inclusion functions #
 
