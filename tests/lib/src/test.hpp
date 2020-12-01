63c8fe6891b8f0447dfcadb7d7af6d08c08c4fb0
Ivan Gagis <igagis@gmail.com>
2020-12-01 03:20:39 +0200

    minor tests improvement

--- tests/lib/src/test.hpp
+++ tests/lib/src/test.hpp
@@ -1,3 +1,17 @@
 #pragma once
 
+#include <iostream>
 
+class test_class{
+
+public:
+	test_class(){
+		int a = 3;
+
+		[&a](){a += 1;}();
+
+		std::cout << "a = " << a << std::endl;
+	}
+
+	test_class(const test_class&) = delete;
+};
