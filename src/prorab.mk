20ff25b13a98ce5d6e71647921846dc28d333d2b
Ivan Gagis <igagis@gmail.com>
2021-06-18 14:48:21 +0300

    split lines

--- src/prorab.mk
+++ src/prorab.mk
@@ -303,7 +303,7 @@ $(.RECIPEPREFIX)$(a)rm -rf $(d)out
 
     # for windows we have to convert windows paths to unix paths using cygpath
     ifeq ($(os),windows)
-        prorab_private_d_file_sed_command = sed -E -i -e "s/^ //g" $$(patsubst %.o,%.d,$$@) \
+        prorab_private_d_file_sed_command = sed -E -i -e "s/^ //g;s/([^ ]) ([^ \])/\1 \\\\\n\2/g" $$(patsubst %.o,%.d,$$@) \
                 && cygpath -f $$(patsubst %.o,%.d,$$@) > $$(patsubst %.o,%.d,$$@).tmp \
                 && mv $$(patsubst %.o,%.d,$$@).tmp $$(patsubst %.o,%.d,$$@) \
                 && sed -E -i -e "s/ \/$$$$/ \\\\/g" $$(patsubst %.o,%.d,$$@) \
